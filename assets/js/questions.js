c096fb478491166e6ea43776330e07ee39755e05
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-17 22:51:44 +0000

    loadHighScore func logic added and correctly loads called with window.addEventListener

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,47 @@
 var questions = [
     {
-        title: 'Fav food?',
-        choices: ['cheese', 'potato', 'noodles'],
-        answer: 'noodles'
+        title: 'Arrays in JavaScript can be used to store...?',
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above'
     },
     {
-        title: 'Fav color?',
-        choices: ['orange', 'purple', 'noir'],
-        answer: 'orange'
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
     },
     {
-        title: 'Fav beer?',
-        choices: ['carling', 'fosters', 'kronenbourg'],
-        answer: 'kronenbourg'
+        title: 'Inside which HTML element do we put the JavaScript?',
+        choices: ['<scripting>', '<js>', '<javascript>', '<script>'],
+        answer: '<script>'
     },
     {
-        title: 'Fav drink?',
-        choices: ['cola', 'lemonade', 'juice'],
-        answer: 'juice'
+        title: 'Where is the correct place to insert a JavaScript?',
+        choices: ['<head> section', '<body> section', 'both <head> and <body> sections are correct'],
+        answer: 'both <head> and <body> sections are correct'
     },
     {
-        title: 'Fav game?',
-        choices: ['tic-tac-toe', 'chess', 'draughts'],
-        answer: 'chess'
+        title: 'The external JavaScript file must contain the <script> tag.',
+        choices: ['true', 'false'],
+        answer: 'false'
+    },
+    {
+        title: 'How does a WHILE loop start?',
+        choices: ['while i = 1 to 10', 'while (i <= 10)', 'while (i <= 10; i++)'],
+        answer: 'while (i <= 10)'
+    },
+    {
+        title: 'How can you add a comment in JavaScript?',
+        choices: ['*This is a comment*', '<!--This is a comment-->', '//This is a comment'],
+        answer: '//This is a comment'
+    },
+    {
+        title: 'What is the correct way to write a JavaScript array?',
+        choices: ['var colors = ["red", "green", "blue"]', 'var colors = (1:"red", 2:"green", 3:"blue")', 'var colors = "red", "green", "blue"'],
+        answer: 'var colors = ["red", "green", "blue"]'
+    },
+    {
+        title: 'Which event occurs when the user clicks on an HTML element?',
+        choices: ['onmouseover', 'onclick', 'onchange', 'onmouseclick'],
+        answer: 'onclick'
     }
 ]
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,9 +2,21 @@ var clearScoresBtn = document.querySelector("#clear");
 var highScores = document.querySelector("#highscores");
 
 // eventlistener to load highscores on page load
+window.addEventListener("load", loadHighScore);
 
 // listener to clearHighScores on clearScoresBtn click
 
 // function to clearHighScores - clear local storage
 
 // function to loadHighScores - obtain from local storage
+function loadHighScore() {
+    var storage = Object.entries(localStorage);
+    storage.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    storage.forEach(element => {
+        var li = document.createElement("li");
+        li.textContent = element[0]+":"+ element[1];
+        highScores.appendChild(li);
+    });
+}
