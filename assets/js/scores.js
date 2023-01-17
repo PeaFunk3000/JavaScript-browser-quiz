9d99f3f75c9c217ffa1474566893d8682701a578
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-17 22:54:44 +0000

    clearHighScores and event listener

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,18 +5,24 @@ var highScores = document.querySelector("#highscores");
 window.addEventListener("load", loadHighScore);
 
 // listener to clearHighScores on clearScoresBtn click
+clearScoresBtn.addEventListener("click", clearHighScores)
 
-// function to clearHighScores - clear local storage
-
+// function to clearHighScores - clear local storage, clear HTML content
+function clearHighScores() {
+    localStorage.clear();
+    highScores.innerHTML = '';
+}
 // function to loadHighScores - obtain from local storage
 function loadHighScore() {
     var storage = Object.entries(localStorage);
-    storage.sort(function(a, b) {
+    // sort storage by i[1] so highest score at the top
+    storage.sort(function (a, b) {
         return b[1] - a[1];
     });
+    //forEach to create li and append
     storage.forEach(element => {
         var li = document.createElement("li");
-        li.textContent = element[0]+":"+ element[1];
+        li.textContent = element[0] + ":" + element[1];
         highScores.appendChild(li);
     });
 }
