70060bc268c6471892a1e1ece97040504d8d8465
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-17 16:31:33 +0000

    questions.js written array of objects temp-content

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: 'Fav food?',
+        choices: ['cheese', 'potato', 'noodles'],
+        answer: 'noodles'
+    },
+    {
+        title: 'Fav color?',
+        choices: ['orange', 'purple', 'noir'],
+        answer: 'orange'
+    },
+    {
+        title: 'Fav beer?',
+        choices: ['carling', 'fosters', 'kronenbourg'],
+        answer: 'kronenbourg'
+    },
+    {
+        title: 'Fav drink?',
+        choices: ['cola', 'lemonade', 'juice'],
+        answer: 'juice'
+    },
+    {
+        title: 'Fav game?',
+        choices: ['tic-tac-toe', 'chess', 'draughts'],
+        answer: 'chess'
+    }
+]
