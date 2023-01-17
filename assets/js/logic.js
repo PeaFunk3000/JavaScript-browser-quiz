a62c52d786c047df6e1bb4075459dc15d676b3b3
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-17 16:42:13 +0000

    queryselect vars defined in logic.js, problem psedocoded defining required funcs

---
+++ assets/js/logic.js
@@ -0,0 +1,32 @@
+var startGame = document.querySelector('#start');
+var timerE = document.querySelector('.timer');
+var endScreen = document.querySelector('#end-screen');
+var finalScoreE = document.querySelector('#final-score');
+var initialScoreE = document.querySelector('#initials');
+var questionsE = document.querySelector('#questions');
+var choicesE = document.querySelector('#choices');
+var submitE = document.querySelector('#submit');
+var feedbackE = document.querySelector('#feedback');
+
+
+var quizIndex = 0;
+var timePerQ = 5;
+var timerCount = questions.length * timePerQ;
+var timerID;
+var score = 0;
+
+var sfxCorrect = new Audio('./assets/sfx/correct.wav');
+var sfxIncorrect = new Audio('./assets/sfx/incorrect.wav');
+
+// Func to startQuiz - start timer calling clockTimer in setInterval, show first question calling getQuestion
+
+// Func to getQuestion - create choice buttons, call questionClick onclick
+
+// Func for questionClick - display Correct/Incorrect on selection, increment to next Question, reduce timer if incorrect
+
+// Func to quizEnd - stop timer, show end screen
+
+// Func for clockTimer - invoked in startQuiz, call quizEnd if runs to 0
+
+// Func to save highScore - record user data
+
